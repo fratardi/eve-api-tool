917c7751e6f0b9ae4bf9af4c05dfaf930b365c4c
Francois TARDIEU <fratardi@e1r6p2.clusters.42paris.fr>
2022-09-15 01:25:35 +0200

    hehehe

--- sample-application/src/app/Services/eve-esi.service.ts
+++ sample-application/src/app/Services/eve-esi.service.ts
@@ -1,51 +1,90 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
 import * as Swagger from '../interfaces'
 
 
 const REFRESH_TOKEN = 'refresh_token';
-const ACCESS_TOKEN = 'access_token';
+const ACCESS_TOKEN  = 'access_token';
+const secretKey     =  	"jcVXo0IZFt5YDr8AJ3Z7cKCDfVijNxKhupOKCQ2I"
+const client_id   	=	"7f45c8124b2640beba3a6902df6832a2";
+
 @Injectable({
   providedIn: 'root'
 })
 export class EveEsiService {
 
+	   token!: Swagger.Tokens;
+   userOwn!: Swagger.UserInfo;
+  constructor(	private http: HttpClient) {}
+
+	base64encodedstring(client_id :string ,  secret  : string){
+		let stringToEncode = client_id+ ":"+secret;
+		return( "Basic "+btoa(stringToEncode))
+	}
 
 
-	 secretKey :string =  	"jcVXo0IZFt5YDr8AJ3Z7cKCDfVijNxKhupOKCQ2I"
-	 client_id :string 	=	"7f45c8124b2640beba3a6902df6832a2";
-   token!: Swagger.Tokens;
-
-  constructor(	private http: HttpClient,) {}
-
-
-
-  init_service(body:any , httpHeaders :HttpHeaders , lol:any)
+  getUserOwn  ()
   {
-    console.log("YOLO"+lol)
+      console.log(this.userOwn)
+    return(this.userOwn);
+  }
+
+
+	generateCurlRequest(encodedSomething : string , base64encodedstring : string){
+		let command =	'curl -XPOST -H ';
+		let header = '"Content-Type:application/json" -H "Authorization:Basic '+ encodedSomething +'" -d ';
+		let payload  = "'"+'{"grant_type":"authorization_code", "code":"'+base64encodedstring+'"}' + "' ";
+		let url = "https://login.eveonline.com/oauth/token"
+	//	console.log( command +header + payload + url  );
+		return ({ encodedSomething: encodedSomething , base64encodedstring : base64encodedstring     })  ;
+	}
+
+
+  init_service(
+    
+    callbackCode :any
+ 
+     )  
+  {
+
+
+    let something =  client_id+ ":" + secretKey;
+		let encodedSomething = btoa(something);
+		let httpHeaders = new HttpHeaders({
+			"Authorization": "Basic " + encodedSomething,
+			'Content-Type': 'application/json',
+		});
+    let infos =	this.generateCurlRequest(encodedSomething, callbackCode.code.toString());
+		let base64encodedstring : string  =infos.base64encodedstring
+
+
+    let body1 = {"grant_type":"authorization_code", "code":""+base64encodedstring+""    }
+		
+
+
+    console.log("YOLO")
     this.http.post('http://localhost:4200/postCodes',
-		body,
+		body1,
 		{ headers: httpHeaders }
 		 )
 		.subscribe(data  => {
 			this.token = data as Swagger.Tokens;
 			console.log("DATA  = ",data )
 			let httpHeaders2 = new HttpHeaders({"Authorization":" Bearer " +  this.token.access_token})	
-			// this.http.get('http://localhost:4200/verify',{headers  : httpHeaders2})
-			// 	.subscribe(e => {
-        localStorage.setItem(ACCESS_TOKEN, this.token.access_token);
-			// 	this.userOwn = e as	 Swagger.UserInfo;
-			// 	console.log(this.userOwn)
-			// 	// this.http.get('http://localhost:4200/latest/characters/'+userOwn.CharacterID+'/standings/',{headers  : httpHeaders2})
-			// 	// .subscribe(e => {console.log("makumbass",e)})
-			// })
+
+      localStorage.setItem(ACCESS_TOKEN, this.token.access_token);
+      localStorage.setItem(REFRESH_TOKEN, this.token.refresh_token);
+
+			this.http.get('http://localhost:4200/verify',{headers  : httpHeaders2})
+				.subscribe(e => {
+				this.userOwn = e as	 Swagger.UserInfo;
+				console.log(e)
+        return(this.userOwn)
+				// this.http.get('http://localhost:4200/latest/characters/'+userOwn.CharacterID+'/standings/',{headers  : httpHeaders2})
+				// .subscribe(e => {console.log("makumbass",e)})
+			})
 		})
-
     console.log("endof" , this.token)
-
-
   }
 
   askRefresh(currentToken: string)
--- sample-application/src/app/second/second.component.ts
+++ sample-application/src/app/second/second.component.ts
@@ -45,80 +45,30 @@ import { EveEsiService } from '../Services/eve-esi.service';
 
 export class SecondComponent implements OnInit {
 
+	userOwn!  :	 Swagger.UserInfo;
 
-	client_id 	=	"7f45c8124b2640beba3a6902df6832a2";
-	secretKey  =  	"jcVXo0IZFt5YDr8AJ3Z7cKCDfVijNxKhupOKCQ2I"
-
-	token : Swagger.Tokens|undefined;
-
-	userOwn : Swagger.UserInfo | undefined;
 	
 	constructor(
-		private http: HttpClient,
 		private route: ActivatedRoute,
 		private esi_service : EveEsiService
-		
 	) { }
 
-	generateCurlRequest(encodedSomething : string , base64encodedstring : string){
-		let command =	'curl -XPOST -H ';
-		let header = '"Content-Type:application/json" -H "Authorization:Basic '+ encodedSomething +'" -d ';
-		let payload  = "'"+'{"grant_type":"authorization_code", "code":"'+base64encodedstring+'"}' + "' ";
-		let url = "https://login.eveonline.com/oauth/token"
-	//	console.log( command +header + payload + url  );
-		return ({ encodedSomething: encodedSomething , base64encodedstring : base64encodedstring     })  ;
+	truc(){
+		console.log("!!!!!!")
+		this.esi_service.getUserOwn();
 	}
-
-	base64encodedstring(client_id :string ,  secret  : string){
-		let stringToEncode = client_id+ ":"+secret;
-		return( "Basic "+btoa(stringToEncode))
-	}
-
-
-	truc()
-	{
-
-		console.log(this.esi_service.token.access_token)
-
-	}
-
 
 
 	ngOnInit(): void {
-	
+		let callbackCode  : any ; 
+		this.route.queryParams.subscribe(e => {
+			callbackCode = e
+			  this.esi_service.init_service( callbackCode)
+			//this.userOwn = 
+		//	this.esi_service.getUserOwn();
 
-	let authorizationToken  : any ; 
-	this.route.queryParams.subscribe(e => authorizationToken = e)
-	let something =  this.client_id+ ":" + this.secretKey;
-	let encodedSomething = btoa(something);
-	let httpHeaders = new HttpHeaders({
-		"Authorization": "Basic " + encodedSomething,
-		'Content-Type': 'application/json',
-	});
-	let infos =	this.generateCurlRequest(encodedSomething, authorizationToken.code.toString());
-	let base64encodedstring : string =infos.base64encodedstring
-	let body = {"grant_type":"authorization_code", "code":""+base64encodedstring+""    }
-	let access_token : Swagger.Tokens;
-	let paths : Swagger.Paths ;
-
-
-	this.esi_service.init_service(    body , httpHeaders  , 	this.route.queryParams.  subscribe(e => authorizationToken = e)  )
-
-
-	// 	let truc : Swagger.Welcome;
-	// 	this.http.get('http://localhost:4200/api').subscribe(e =>{ truc = e as Swagger.Welcome
-	
-	// 	console.log(truc.paths['/markets/prices/'])
-	
-	// 
-	let 	truc = this.esi_service.token as Swagger.Tokens;
-	
-	//this.http.post('http://localhost:4200/xway', body).subscribe(e => console.log("makumbass",e))
-
-//this.http.get('http://localhost:4200/api').subscribe(e => console.log("makumba",e))
-
-	console.log( "HELLOOOO" + truc )
-
+		})
 	}
-
 }
+
+
