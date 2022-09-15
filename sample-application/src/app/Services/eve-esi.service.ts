e8b786c35d247389c902a6803beecac53ea59852
Francois TARDIEU <fratardi@e1r9p2.clusters.42paris.fr>
2022-09-16 01:08:58 +0200

    tokent refresh time ok

--- sample-application/src/app/Services/eve-esi.service.ts
+++ sample-application/src/app/Services/eve-esi.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import * as Swagger from '../interfaces'
 
 const REFRESH_TOKEN = 	'refresh_token';
@@ -11,6 +11,7 @@ const client_id   	=	"7f45c8124b2640beba3a6902df6832a2";
 @Injectable({
   providedIn: 'root'
 })
+
 export class EveEsiService {
 
 
@@ -20,10 +21,14 @@ export class EveEsiService {
 	base64string:any;
 
   	constructor(
-		private http: HttpClient,
-		private router: Router,
-//		private route: ActivatedRoute,
+		private http	: HttpClient,
+		private router	: Router,
 	) {}
+
+
+
+	
+
 
 	base64encodedstring(client_id :string ,  secret  : string){
 		let stringToEncode = client_id+ ":"+secret;
@@ -42,10 +47,6 @@ export class EveEsiService {
 	}
 
 	generateCurlRequest(encodedSomething : string , base64encodedstring : string){
-		let command =	'curl -XPOST -H ';
-		let header = '"Content-Type:application/json" -H "Authorization:Basic '+ encodedSomething +'" -d ';
-		let payload  = "'"+'{"grant_type":"authorization_code", "code":"'+base64encodedstring+'"}' + "' ";
-		let url = "https://login.eveonline.com/oauth/token"
 		return ({ encodedSomething: encodedSomething , base64encodedstring : base64encodedstring     })  ;
 	}
 
@@ -58,10 +59,10 @@ export class EveEsiService {
 		});
 		let infos =	this.generateCurlRequest(encodedSomething, callbackCode.code.toString());
 		let base64encodedstring : string  =infos.base64encodedstring
-		
-			this.base64string = base64encodedstring;
-		
-		let body1 = {"grant_type":"authorization_code", "code": "" + base64encodedstring + ""}
+		this.base64string = base64encodedstring;
+		let body1 = {
+			"grant_type":"authorization_code", "code": "" + base64encodedstring + ""
+		}
 		this.http.post(  this.hostpoint +  '/postCodes',
 			body1,
 			{ headers: httpHeaders }
@@ -78,121 +79,45 @@ export class EveEsiService {
 				.subscribe(e => {
 				this.userOwn = e as	 Swagger.UserInfo;
 				console.log(e , this.token.expires_in * 60)
-				this.refreshWhenexpired(this.token.expires_in * 60) 
+				this.refreshWhenexpired() 
 				this.router.navigate(['third-component']);
 				return(this.userOwn)
 			})
 		})
 	}
 
-	refreshWhenexpired(duration:any ) {
-		console.log("hello-i-am-refresh-service", duration )
-
-	//	while(1){
-
-		//	setTimeout(this., 414);
-
-		// this.stupidGet()
-
-//	this.refreshToken();
-
-	////	}
-	
-	//	setTimeout(function,23)
-		setInterval(() =>    this.refreshToken(), 2000  )
+	refreshWhenexpired( ) {
+		setInterval(() =>  this.refreshToken(), 19 * 1000 * 60  )
 	}
-
-
-	stupidGet()
-	{
-		this.http.get("https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0")
-			.subscribe(data => console.log(data))
-
-	}
-
-
 
   	refreshToken(){
-		console.log("hello-i-am-refreshToken")
-
-
-
-
-
 		let something =  client_id+ ":" + secretKey;
 		let encodedSomething = btoa(something);
-
-		//let infos =	this.generateCurlRequest(encodedSomething, callbackCode.code.toString());
-		//let base64encodedstring : string  =infos.base64encodedstring
-
-
-
-
 		let httpHeaders = new HttpHeaders({
 			"Authorization": "Basic " + encodedSomething,
-			'Content-Type': 'application/json',
-		});
-		console.log("hello-i-am-refreshToken",  localStorage.getItem(REFRESH_TOKEN)?.valueOf())
-
-		let body1 = {	
-						"grant_type":"refresh_token",
-						"refresh_token": "" +  localStorage.getItem(REFRESH_TOKEN)?.valueOf() + ""
-					}
-
-
-		console.log(
-			 this.hostpoint +  '/postCodes',
-			body1,
-			{ headers: httpHeaders })
-
-
-		this.http.post(  this.hostpoint +  '/postCodes',
-		body1,
-		{ headers: httpHeaders }
-	)
-	.subscribe(data  => {
-		this.token = data as Swagger.Tokens;
-		console.log("DATA  = ",data )
-		let httpHeaders2 = new HttpHeaders(
-			{"Authorization":" Bearer " +  this.token.access_token}
+			'Content-Type': 'application/json',});
+		let body = {	
+			"grant_type":"refresh_token",
+			"refresh_token": "" +  localStorage.getItem(REFRESH_TOKEN)?.valueOf() + ""}
+		this.http.post( 
+			this.hostpoint +  '/postCodes',
+			body,
+			{headers: httpHeaders }
 		)
-		  localStorage.setItem(ACCESS_TOKEN, this.token.access_token);
-		  localStorage.setItem(REFRESH_TOKEN, this.token.refresh_token);
-		this.http.get(this.hostpoint+ '/verify',{headers  : httpHeaders2})
-			.subscribe(e => {
-			this.userOwn = e as	 Swagger.UserInfo;
-		//	console.log(e , this.token.expires_in * 60)
-		//	this.refreshWhenexpired(this.token.expires_in * 60) 
-		//	this.router.navigate(['third-component']);
-			return(this.userOwn)
+		.subscribe(data  => {
+			this.token = data as Swagger.Tokens;
+			console.log("DATA  = ",data )
+			let httpHeaders2 = new HttpHeaders(
+				{"Authorization":" Bearer " +  this.token.access_token}
+			)
+		  	localStorage.setItem(ACCESS_TOKEN, this.token.access_token);
+		  	localStorage.setItem(REFRESH_TOKEN, this.token.refresh_token);
+			this.http.get(this.hostpoint+ '/verify',{headers  : httpHeaders2})
+				.subscribe(e => {
+					this.userOwn = e as	 Swagger.UserInfo;
+					return;
+				})
 		})
-	})
-
-
-
-
-
-
-
-
-
-	//	let body1 = {"grant_type":"authorization_code", "code": "" + base64encodedstring + ""}
-
-	//	let body1 = {"grant_type":"authorization_code",   "refresh_token": "" + localStorage.getItem(REFRESH_TOKEN) + ""}
-		//let body = {"grant_type":"authorization_code",  : currentToken};
-	//	console.log('updating here ', body1, this.hostpoint, this.hostpoint +  '/postCodes'	);
-		
-
-	// 	this.http.get(  this.hostpoint +  '/postCodes',
-	// //	body1,
-	// 	//{ headers: httpHeaders }
-	// )
-	// .subscribe(data  => {
-
-	// 	console.log( "hellooo maybe data but not checck ",  )
-
-	// })
-
 	}
 
   	getToken(): any {
@@ -200,9 +125,6 @@ export class EveEsiService {
  	}
 
 	getRefreshToken(): any {
-
-		console.log("getrefreshtoken ", localStorage.getItem(REFRESH_TOKEN))
-
 		return localStorage.getItem(REFRESH_TOKEN);
   	}
 
