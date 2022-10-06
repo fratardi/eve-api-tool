5b16880fed6da3bc0c26e5f6103c0e6609604919
Francois TARDIEU <fratardi@e1r5p1.clusters.42paris.fr>
2022-10-06 03:25:01 +0200

    okd

--- sample-application/src/app/kill-widget/kill-widget.component.ts
+++ sample-application/src/app/kill-widget/kill-widget.component.ts
@@ -1,20 +1,59 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+
+
+interface truc
+{
+  attackers: any,
+  killmail_id :  any,
+  killmail_time : any,
+  solar_system_id :any,
+  victim : any,
+  zkb :any,
+
+}
+
 @Component({
   selector: 'app-kill-widget',
   templateUrl: './kill-widget.component.html',
   styleUrls: ['./kill-widget.component.less']
 })
 
+
+
+
 export class KillWidgetComponent implements OnInit {
 
 
   @Input() data: any[] | undefined;
 
-  constructor() { }
+  valeur :truc | undefined;
+
+
+  constructor() {
+    this.valeur     = this.data as unknown as truc;
+
+   }
 
   ngOnInit(): void {
-    console.log(this.data)
+    console.log( "MICHaEL" ,this.data)
+    this.lol();
   }
+
+  lol()
+  {
+    this.valeur     = this.data as unknown as truc;
+
+   // this.valeur = this.data as truc;
+
+    console.log(this);
+
+  }
+  MediaStreamAudioDestinationNode(){
+
+  }
+
+
+
 
 }
