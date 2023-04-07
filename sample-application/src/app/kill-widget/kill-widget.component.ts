b44abb5d1c98d8184f244efb17423e1119c0f937
Your Name <you@example.com>
2023-04-07 14:19:25 +0000

    removed styles but added the victim resolution

--- sample-application/src/app/kill-widget/kill-widget.component.ts
+++ sample-application/src/app/kill-widget/kill-widget.component.ts
@@ -2,14 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { EveEsiService } from '../Services/eve-esi.service';
 import { bufferTime } from 'rxjs';
 
-interface truc{
-	attackers: any,
-	killmail_id :  any,
-	killmail_time : any,
-	solar_system_id :any,
-	victim : any,
-	zkb :any,
-}
+
 
 interface truc {
 	attackers: any,
@@ -37,8 +30,10 @@ export class KillWidgetComponent implements OnInit {
 	final : any = {};
 	hasInit : boolean = false;
 	resolvedList : any = [];
-	processedKillReport: any;
+	processedAttack: any;
 	attackerSolved : any = [];
+	
+	victimSolved : any = [];
 
 	constructor(
 	 private  eveEsiService : EveEsiService
@@ -47,44 +42,23 @@ export class KillWidgetComponent implements OnInit {
 	ngOnInit(): void {
 		console.log("init widget ", this.data)
 		this.zKillReport = this.data as unknown as truc;
-		this.getKillReport();
-		this.eveEsiService.getKillHashId( this.data).subscribe(e =>{
 
+		this.eveEsiService.getKillHashId( this.data).subscribe(e =>{
 		console.log("Subscription ok"    , e  )
 		this.eveKillReport =e 
-		 this.collectIds( e  , this.itemIds)
-		
-    console.log('processedKillReport:', this.processedKillReport);
+		this.eveEsiService.getNamesInfoFromId(this.removeDuplicates( this.collectIds( e  , this.itemIds)))
+			.subscribe((data: string[]) => {
+				console.log("getNamesInfoFromId(" , data);
+				this.solvedTabs.push(data);
+			//	this.resolveIds( this.eveKillReport , this.solvedTabs  ) 
+				this.attackerSolved = this. generateAttackBattleReportSummary(this.eveKillReport, [data]);
+				this.victimSolved = this. generateVictimBattleReportSummary(this.eveKillReport, data);
 
 
+				this.processedAttack =  this.attackerSolved;
+			});
+    		console.log('processedKillReport:', this.processedAttack);
 	})
-	 // this.collectIds(this.zKillReport, this.itemIds); // Collect IDs and store them in itemIds array
-	}
-
-
-	resolveIds(unresolved: any, solved: any): any {
-		const resolved: any = {};
-
-		for (const key in unresolved) {
-			if (unresolved.hasOwnProperty(key)) {
-				if (Array.isArray(unresolved[key])) {
-					resolved[key] = unresolved[key].map((item: any) => {
-						if (typeof item === "object") {
-							return this.resolveIds(item, solved);
-						} else {
-							return solved[item] || item;
-						}
-					});
-				} else if (typeof unresolved[key] === "object") {
-					resolved[key] = this.resolveIds(unresolved[key], solved);
-				} else {
-					resolved[key] = solved[unresolved[key]] || unresolved[key];
-				}
-			}
-		}
-	//	console.log("resol ved ", resolved , "💖💖💖💖😘😘😘")
-		this.resolvedList.push(resolved)
-		return resolved;
 	}
 
 
@@ -92,8 +66,36 @@ export class KillWidgetComponent implements OnInit {
 	{
 	//	console.log("α" ,JSON.stringify(this.eveKillReport), "δ" ,JSON.stringify(this.solvedTabs));
 		//const linkedJson = this. generateAttackBattleReportSummary(this.eveKillReport, this.solvedTabs);
-			console.log(this.processedKillReport);
+		
+
+
+			console.log(this.processedAttack , this);
 	}
+
+	generateVictimBattleReportSummary(data: any, names: any) 
+	{
+		console.log(  "generateVictimBattleReportSummary(", data , names )
+		let  returnValue : any =[]; 
+		names.forEach((item : any) => {
+		//	console.log(item.id , data.victim.alliance_id)
+			if(item.id == data.victim.character_id)	
+				returnValue.character = item;
+			if(item.id == data.victim.alliance_id)	
+				returnValue.alliance = item;
+			if(item.id == data.victim.corporation_id)	
+				returnValue.corporation = item;
+			if(item.id == data.victim.ship_type_id)	
+				returnValue.ship = item;
+			 if(item.id == data.solar_system_id )	
+			 returnValue.location = item;
+	
+		})
+		returnValue.damage = data.victim.damage_taken
+		console.log(  "🐸returnValue", returnValue)
+		return  returnValue
+
+	}
+
 
 	generateAttackBattleReportSummary(data: any, names: any) : any[] {
 	let  returnValue : any[] = []; 
@@ -110,6 +112,8 @@ export class KillWidgetComponent implements OnInit {
 								attacker.corporation  = ligne
 							if(ligne.category =="inventory_type" && element.weapon_type_id == ligne.id)
 								attacker.item = ligne
+							if(ligne.category =="inventory_type" && element.ship_type_id == ligne.id)
+								attacker.ship= ligne
 							if(ligne.category =="alliance"&& element.alliance_id == ligne.id)
 								attacker.alliance = ligne
 					});
@@ -120,10 +124,6 @@ export class KillWidgetComponent implements OnInit {
 		return  returnValue
 	}
 
-	getKillReport(){
-		this.hasInit = true;
-			// this.eveKillReport =  this.eveEsiService.getCCPKillReport(this.zKillReport?.killmail_id , this.zKillReport?.zkb.hash );
-	}
 
 	setDestination()
 	{
@@ -131,40 +131,40 @@ export class KillWidgetComponent implements OnInit {
 		console.log(this);
 	}
 
-	collectIds(obj: any, ids: number[]): void {
+	collectIds(obj: any, ids: number[]): any {
 		if (obj.attackers && Array.isArray(obj.attackers)) {
 			for (const attacker of obj.attackers) {
-				if (attacker.character_id) ids.push(attacker.character_id);
-				if (attacker.corporation_id) ids.push(attacker.corporation_id);
-				if (attacker.ship_type_id) ids.push(attacker.ship_type_id);
-				if (attacker.weapon_type_id) ids.push(attacker.weapon_type_id);
-				if (attacker.alliance_id) ids.push(attacker.alliance_id);
+				if (attacker.character_id) 
+					ids.push(attacker.character_id);
+				if (attacker.corporation_id)
+					ids.push(attacker.corporation_id);
+				if (attacker.ship_type_id)
+					ids.push(attacker.ship_type_id);
+				if (attacker.weapon_type_id)
+					ids.push(attacker.weapon_type_id);
+				if (attacker.alliance_id)
+					ids.push(attacker.alliance_id);
 			}
 		}
 		if (obj.solar_system_id) {
 			ids.push(obj.solar_system_id);
 		}
 		if (obj.victim) {
-			if (obj.victim.character_id) ids.push(obj.victim.character_id);
-			if (obj.victim.corporation_id) ids.push(obj.victim.corporation_id);
-			if (obj.victim.ship_type_id) ids.push(obj.victim.ship_type_id);
-
+			if (obj.victim.character_id)
+				ids.push(obj.victim.character_id);
+				if (obj.victim.alliance_id)
+				ids.push(obj.victim.alliance_id);
+			if (obj.victim.corporation_id)
+				ids.push(obj.victim.corporation_id);
+			if (obj.victim.ship_type_id)
+				ids.push(obj.victim.ship_type_id);
 			if (obj.victim.items && Array.isArray(obj.victim.items)) {
 				for (const item of obj.victim.items) {
 					if (item.item_type_id) ids.push(item.item_type_id);
 				}
 			}
 		}
-		this.eveEsiService.getNamesInfoFromId(this.removeDuplicates(ids))
-			.subscribe((data: string[]) => {
-				console.log("getNamesInfoFromId(" , data);
-				this.solvedTabs.push(data);
-				this.resolveIds( this.eveKillReport , this.solvedTabs  ) 
-				this.attackerSolved = this. generateAttackBattleReportSummary(this.eveKillReport, this.solvedTabs);
-				this.processedKillReport = this.attackerSolved
-			});
-
-		console.log("Solved", this.attackerSolved)
+		return(ids)
 	}
 
 	removeDuplicates(arr: any[]): any[] {
